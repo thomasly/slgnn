c68947d4c9793fc50f32311e5fc2bba986dc2d72
Yang Liu <thomasliuy@gmail.com>
2020-04-03 21:15:48 -0400

    save and load hdf5

--- slgnn/data_processing/zinc_to_hdf5.py
+++ slgnn/data_processing/zinc_to_hdf5.py
@@ -1,9 +1,12 @@
 import os
 import random
 import pickle as pk
+import logging
 
 from chemreader.readers.readmol2 import Mol2
 import h5py
+import numpy as np
+from scipy import sparse
 from tqdm import tqdm
 
 
@@ -15,10 +18,13 @@ class ZincToHdf5:
 
     def __init__(self, mol2blocks: list, n_mols=None):
         self._mol2s = mol2blocks
-        self._n_mols = n_mols
+        if n_mols is None:
+            self._n_mols = len(mol2blocks)
+        else:
+            self._n_mols = n_mols
 
     @classmethod
-    def from_files(cls, fpaths: list):
+    def from_files(cls, fpaths: list, shuffle=False):
         for f in fpaths:
             if f.split(".")[-1] not in ["mol2", "gz"]:
                 raise ValueError("{} is not a valid path.".format(f))
@@ -29,57 +35,99 @@ class ZincToHdf5:
             blocks = mol2.mol2_blocks
             mol2blocks.extend(blocks)
             n_mols += mol2.n_mols
+        if shuffle:
+            shuffle(mol2blocks)
         return cls(mol2blocks, n_mols)
 
     @classmethod
-    def random_sample(cls, n_samples, dir_path=None):
+    def _get_index(cls, dir_path, verbose):
+        if not os.path.exists(os.path.join(dir_path, "index")):
+            logging.info("Generating index...")
+            index = cls.indexing(dir_path, verbose=verbose)
+        else:
+            with open(os.path.join(dir_path, "index"), "rb") as f:
+                index = pk.load(f)
+        return index
+
+    @classmethod
+    def _sample_all(cls, dir_path, mol2blocks, verbose):
+        files = os.scandir(dir_path)
+        n_mols = 0
+        for f in (tqdm(list(files)) if verbose else files):
+            if f.name.split(".")[-1] not in ["gz", "mol2"]:
+                continue
+            mol2 = Mol2(f.path)
+            mol2blocks.extend(mol2.mol2_blocks)
+            n_mols += mol2.n_mols
+        random.shuffle(mol2blocks)
+        return n_mols
+
+    @classmethod
+    def random_sample(cls, n_samples, dir_path=None, verbose=True):
         if dir_path is None:
             dir_path = "data"
-        files = list(os.scandir(dir_path))
-        mol2blocks = set()
-        while len(mol2blocks) < n_samples:
-            f = random.choice(files)
-            while not f.name.split(".")[-1] in ["gz", "mol2"]:
-                f = random.choice(files)
-            mol2 = Mol2(f.path)
-            mol2blocks.add(random.choice(mol2.mol2_blocks))
-
-    def _count_mols(self):
-        counter = 0
-        for f in tqdm(self._fpaths):
-            if not f.split(".")[-1] in ["gz", "mol2"]:
-                continue
-            counter += Mol2(f).n_mols
-        self._n_mols = counter
+        index = cls._get_index(dir_path, verbose=verbose)
+        # Get Mol2Blocks randomly
+        mol2blocks = list()
+        # n_samples is larger than the number of samples in the dir_path
+        if n_samples > index["total"]:
+            logging.warning(
+                "{} does not have enough samples. Got {}/{}".format(
+                    dir_path, index["total"], n_samples))
+            n_mols = cls._sample_all(dir_path, mol2blocks, verbose=verbose)
+            return cls(mol2blocks, n_mols)
+        # n_samples is smaller than the number of samples in the dir_path
+        samples = random.sample(list(range(index["total"])), n_samples)
+        for sample in (tqdm(samples) if verbose else samples):
+            file_path = os.path.join(dir_path, index[sample][0])
+            mol2blocks.append(Mol2(file_path).mol2_blocks[index[sample][1]])
+        return cls(mol2blocks, n_samples)
 
     @property
     def n_mols(self):
-        if self._n_mols is None:
-            respond = input(
-                "This may take a while. Continue? [Y/n]")
-            if respond.strip().upper() in ["", "Y", "YES"]:
-                self._count_mols()
-            else:
-                return
         return self._n_mols
 
-    def __call__(self, out_path: str):
-        return self.save_hdf5(out_path)
-
     def save_hdf5(self, out_path):
-        if self._n_mols is None:
-            self._count_mols()
         h5f = h5py.File(out_path, "w")
-        h5f.create_dataset("atom_features")
-        h5f.create_dataset("bond_features")
-        h5f.create_dataset("adjacency_matrices")
+        dt = h5py.string_dtype(encoding="utf-8")
+        smiles = list()
+        a_matrices = list()
+        atom_feats = list()
+        bond_feats = list()
+        for block in self._mol2s:
+            smiles.append(block.to_smiles(isomeric=True))
+            graph = block.to_graph(sparse=True, pad_atom=70, pad_bond=70)
+            a_matrices.append(graph["adjacency"])
+            atom_feats.append(graph["atom_features"])
+            bond_feats.append(graph["bond_features"])
+        h5f.create_dataset("smiles",
+                           data=np.array(smiles).astype(bytes),
+                           dtype=dt)
+        h5f.create_dataset("atom_features",
+                           data=np.array(atom_feats),
+                           dtype=np.float16)
+        h5f.create_dataset("bond_features",
+                           data=np.array(bond_feats),
+                           dtype=np.int8)
+        # Save sparse adjacency matrices
+        adj = h5f.create_group("adjacency_matrices")
+        for i, mat in enumerate(a_matrices):
+            adj.create_dataset("data_{}".format(i), data=mat.data)
+            adj.create_dataset("indptr_{}".format(i), data=mat.indptr)
+            adj.create_dataset("indices_{}".format(i), data=mat.indices)
+            adj.attrs["shape_{}".format(i)] = mat.shape
+        h5f.close()
 
     @staticmethod
-    def indexing(dir_path):
+    def indexing(dir_path, verbose=True):
         """ Indexing the files. Speed up hdf5 file creation.
 
         Args:
             dir_path (str): path to the directory storing all the .mol2 files.
+            verbose (bool): if to show the progress bar.
+
+        Return:
+            dict: the generated index dict
 
         Output:
             A binary index file under the dir_path.
@@ -87,7 +135,7 @@ class ZincToHdf5:
         files = list(os.scandir(dir_path))
         index = dict()
         indexer = 0
-        for f in tqdm(files):
+        for f in (tqdm(files) if verbose else files):
             if not f.name.split(".")[-1] in ["gz", "mol2"]:
                 continue
             mol2 = Mol2(f.path)
@@ -98,3 +146,24 @@ class ZincToHdf5:
         index["total"] = indexer
         with open(os.path.join(dir_path, "index"), "wb") as index_f:
             pk.dump(index, index_f)
+        return index
+
+
+class Hdf5Loader:
+
+    def __init__(self, path):
+        self.path = path
+
+    def load_adjacency_matrices(self, n):
+        h5f = h5py.File(self.path, "r")
+        adj = h5f["adjacency_matrices"]
+        matrices = list()
+        for i in range(n):
+            mat = sparse.csr_matrix(
+                (adj["data_{}".format(i)][:],
+                 adj["indices_{}".format(i)][:],
+                 adj["indptr_{}".format(i)][:]),
+                adj.attrs["shape_{}".format(i)])
+            matrices.append(mat)
+        h5f.close()
+        return matrices
