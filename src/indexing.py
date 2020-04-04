d3a814831ac5481fe953893d4cf7cf485b8d5fbb
Yang Liu <thomasliuy@gmail.com>
2020-04-03 21:16:15 -0400

    add progress bar option

--- src/indexing.py
+++ src/indexing.py
@@ -4,6 +4,8 @@ from slgnn.data_processing import zinc_to_hdf5
 
 
 parser = ArgumentParser()
-parser.add_argument("--path")
+parser.add_argument("-p", "--path", help="Path to the directory.")
+parser.add_argument("-b", "--progress-bar", action="store_true",
+                    help="Show progress bar.")
 args = parser.parse_args()
-zinc_to_hdf5.ZincToHdf5.indexing(args.path)
+zinc_to_hdf5.ZincToHdf5.indexing(args.path, args.progress_bar)
