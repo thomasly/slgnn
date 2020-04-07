83834cb0cf0c3702c42ae896e88c3f2849adf520
Yang Liu <thomasliuy@gmail.com>
2020-04-07 10:51:31 -0400

    Decoder model

---
+++ slgnn/models/decoder/model.py
@@ -0,0 +1,22 @@
+from torch import nn
+import torch.functional as F
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, n_feat, n_hid, n_out, dropout):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, n_hid, (1, n_feat), stride=1, padding=0)
+        self.conv2 = nn.Conv2d(1, n_out, (1, n_hid), stride=1, padding=0)
+        self.dense = nn.Linear(n_out, n_out)
+        self.dropout = dropout
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x.transpose_(1, 3)
+        x = F.relu(self.conv2(x))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = x.flatten(start_dim=1)
+        x = self.dense(x)
+        return F.sigmoid(x)
