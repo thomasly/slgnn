1be6502b63f45ea1f8b963eecd9ceafaed16e085
Yang Liu <thomasliuy@gmail.com>
2020-04-12 10:39:01 -0400

    test passes

--- slgnn/tests/test_gcn_utils.py
+++ slgnn/tests/test_gcn_utils.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
 from slgnn.models.gcn.utils import get_filtered_fingerprint
-from PyFingerprint.All_Fingerprint import get_fingerprint
 
 
 class TestPubchemFingerprint(TestCase):
@@ -11,9 +10,5 @@ class TestPubchemFingerprint(TestCase):
                   "([C@H]([C@](O1)(C#N)C2=CC=C3N2N=CN=C3N)O)O)OC4=CC=CC=C4"
 
     def test_fp_len(self):
-        pubchem_fp = get_fingerprint(
-            self.sm, fp_type="pubchem", output='vector')
-        filtered_fp, length = get_filtered_fingerprint(self.sm)
-        self.assertEqual(len(filtered_fp), length)
-        self.assertLess(length, len(pubchem_fp))
-        self.assertEqual(length, 740)
+        filtered_fp = get_filtered_fingerprint(self.sm)
+        self.assertEqual(len(filtered_fp), 740)
