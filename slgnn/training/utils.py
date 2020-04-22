c1198a691ec46f445b8a89d974ce063e779f3315
Yang Liu <thomasliuy@gmail.com>
2020-04-22 12:38:26 -0400

    plot reconstructed fingerprint

---
+++ slgnn/training/utils.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+
+def plot_reconstruct(decoder, encoder, features, adj, labels, index, output):
+    fig, axes = plt.subplots(2, 1, figsize=(8., 12.))
+    reconst = decoder(
+        encoder(
+            features[index].cuda(), adj[index].cuda())).detach().cpu().numpy()
+    ax1, ax2 = axes.flatten()
+    ax1.bar(list(range(reconst.shape[1])), reconst[0, :])
+    ax1.set_xlabel("Reconstructed Fingerprint")
+    ax2.bar(list(range(reconst.shape[1])), labels[index])
+    ax2.set_xlabel("PubChem Fingerprint")
+    fig.savefig(output, dpi=300, bbox_inches="tight")
