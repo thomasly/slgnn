340961392f7dd4b6141d7a1487835dc07463a5df
Yang Liu <thomasliuy@gmail.com>
2020-04-28 17:15:04 -0400

    use sampled_smiles instead of hdft

--- src/train_gcn.bat
+++ src/train_gcn.bat
@@ -1,6 +1,6 @@
 python -m slgnn.training.train_gcn ^
---encoder-epochs 50 ^
---classifier-epochs 50 ^
+--encoder-epochs 10 ^
+--classifier-epochs 10 ^
 --encoder-lr 0.0001 ^
 --classifier-lr 0.0001 ^
---encoder-data data/ZINC/zinc_ghose_1000.hdf5
+--encoder-data data/ZINC/sampled_smiles_10000.txt
