f41c4373600b90a868e97b9dd62fe9e7c0c37e84
Yang Liu <thomasliuy@gmail.com>
2020-04-28 13:12:56 -0400

    testing passed

--- slgnn/tests/test_gcn_utils.py
+++ slgnn/tests/test_gcn_utils.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
 from slgnn.models.gcn.utils import get_filtered_fingerprint
+from slgnn.models.gcn.utils import load_encoder_data, load_encoder_txt_data
+import torch
+
+from slgnn.config import PAD_ATOM
 
 
 class TestPubchemFingerprint(TestCase):
@@ -12,3 +16,26 @@ class TestPubchemFingerprint(TestCase):
     def test_fp_len(self):
         filtered_fp = get_filtered_fingerprint(self.sm)
         self.assertEqual(len(filtered_fp), 740)
+
+
+class TestEncoderLoader(TestCase):
+
+    def test_txt_loader(self):
+        dpath = "test_data/sampled_smiles_100.txt"
+        train, valid, len_fp = load_encoder_txt_data(dpath)
+        self.assertEqual(len_fp, 740)
+        self.assertIsInstance(train, dict)
+        self.assertIsInstance(valid, dict)
+        self.assertAlmostEqual(train["features"].shape[0],
+                               valid["features"].shape[0]*9,
+                               delta=9)
+        self.assertEqual(train["features"].shape, (90, PAD_ATOM, 56))
+        self.assertIsInstance(train["features"], torch.FloatTensor)
+        self.assertIsInstance(train["adj"][0], torch.sparse.FloatTensor)
+        self.assertIsInstance(train["labels"], torch.FloatTensor)
+        train2, valid2, len_fp2 = load_encoder_data(dpath, type_="txt")
+        self.assertEqual(train["features"].shape, train2["features"].shape)
+        self.assertEqual(len(train["adj"]), len(train2["adj"]))
+        self.assertEqual(valid["features"].shape, valid2["features"].shape)
+        self.assertEqual(len(valid["adj"]), len(valid2["adj"]))
+        self.assertEqual(len_fp, len_fp2)
