77df77600bb6c613eb9e0b03a4d1f145da7edb8e
Yang Liu <thomasliuy@gmail.com>
2020-04-30 22:32:30 -0400

    training script

--- src/train_gcn.bat
+++ src/train_gcn.bat
@@ -3,4 +3,4 @@ python -m slgnn.training.train_gcn ^
 --classifier-epochs 10 ^
 --encoder-lr 0.0001 ^
 --classifier-lr 0.0001 ^
---encoder-data data/ZINC/sampled_smiles_10000.txt
+--encoder-data data/ZINC/sampled_smiles_100000.txt
