69351ad745eaa6f14135f1ab1d1313a092b8a1af
Yang <thomasliuy@gmail.com>
2020-05-04 17:00:35 -0400

    fix import name

--- src/sample_smiles.py
+++ src/sample_smiles.py
@@ -1,26 +1,41 @@
 from argparse import ArgumentParser
-from slgnn.data_processing.sample_smiles import SmilesSampler
+from slgnn.data_processing.zinc_sample_smiles import SmilesSampler
 
 
 class SamplerArguments(ArgumentParser):
-
     def __init__(self):
         super().__init__()
-        self.add_argument("-s", "--source", required=True,
+        self.add_argument("-s",
+                          "--source",
+                          required=True,
                           help="Path to the source file.")
-        self.add_argument("-o", "--output", required=True,
+        self.add_argument("-o",
+                          "--output",
+                          required=True,
                           help="Path to save the output.")
-        self.add_argument("-n", "--num-samples", required=True, type=int,
+        self.add_argument("-n",
+                          "--num-samples",
+                          required=True,
+                          type=int,
                           help="Number of samples to sample from the source "
-                               "file.")
-        self.add_argument("-f", "--filter", action="store_true",
+                          "file.")
+        self.add_argument("-f",
+                          "--filter",
+                          action="store_true",
                           help="Filter similar SMILES.")
-        self.add_argument("-v", "--verbos", action="store_true",
+        self.add_argument("-v",
+                          "--verbos",
+                          action="store_true",
                           help="Show progress bar.")
-        self.add_argument("-nh", "--no-header", action="store_false",
+        self.add_argument("-nh",
+                          "--no-header",
+                          action="store_false",
                           help="Add this tag if the source file does not have "
-                               "header.")
-        self.add_argument("-t", "--threshold", type=float, default=0.85,
+                          "header.")
+        self.add_argument("-t",
+                          "--threshold",
+                          type=float,
+                          default=0.85,
                           help="Threshold used to filter out similar SMILES.")
 
 
