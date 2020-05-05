fb1005e0cb477785cd27efae2ad65e345d26c5ef
Yang Liu <thomasliuy@gmail.com>
2020-05-05 12:28:35 -0400

    one dense layer decoder

--- slgnn/models/decoder/model.py
+++ slgnn/models/decoder/model.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch.nn.functional as F
 
 from slgnn.config import PAD_ATOM
 
@@ -7,10 +8,11 @@ class Decoder(nn.Module):
 
     def __init__(self, n_feat, n_hid, n_out, dropout):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, n_hid, (1, n_feat), stride=1, padding=0)
-        self.conv2 = nn.Conv2d(1, n_out, (1, n_hid), stride=1, padding=0)
-        # self.dense = nn.Linear(n_out * PAD_ATOM, n_out)
+        # self.conv1 = nn.Conv2d(1, n_hid, (1, n_feat), stride=1, padding=0)
+        # self.conv2 = nn.Conv2d(1, n_out, (1, n_hid), stride=1, padding=0)
         self.dense = nn.Linear(n_feat * PAD_ATOM, n_out)
+        # self.dense1 = nn.Linear(n_feat * PAD_ATOM, n_hid)
+        # self.dense2 = nn.Linear(n_hid, n_out)
         self.dropout = dropout
 
     def forward(self, x):
@@ -21,5 +23,7 @@ class Decoder(nn.Module):
         # x = F.relu(self.conv2(x))
         # x = F.dropout(x, self.dropout, training=self.training)
         x = x.flatten(start_dim=1)
+        # x = F.relu(self.dense1(x))
+        # x = F.dropout(x, self.dropout, training=self.training)
         x = self.dense(x)
         return x
