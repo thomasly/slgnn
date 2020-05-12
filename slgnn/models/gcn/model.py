90bdaf7dc151b5e649ae7ef81aa63b2c8f9f1f93
Yang Liu <thomasliuy@gmail.com>
2020-05-11 22:53:01 -0400

    add GIN model

--- slgnn/models/gcn/model.py
+++ slgnn/models/gcn/model.py
@@ -1,5 +1,10 @@
+import torch
 import torch.nn as nn
+from torch.nn import BatchNorm1d
+from torch.nn import Sequential, Linear, ReLU
 import torch.nn.functional as F
+from torch_geometric.nn import GinConv, global_add_pool, global_mean_pool
+
 from slgnn.models.gcn.layers import GraphConvolution
 
 
@@ -17,3 +22,68 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return x
+
+
+class GIN(nn.Module):
+
+    def __init__(self, dim_features, dim_target, config):
+        super().__init__()
+
+        self.config = config
+        self.dropout = config["dropout"]
+        self.embeddings_dim = [
+            config["hidden_units"][0]] + config["hidden_units"]
+        self.no_layers = len(self.embeddings_dim)
+        self.first_h = []
+        self.nns = []
+        self.cons = []
+        self.linears = []
+
+        train_eps = config["train_eps"]
+        if config["aggregation"] == "sum":
+            self.pooling = global_add_pool
+        elif config["aggregation"] == "mean":
+            self.pooling = global_mean_pool
+
+        for layer, out_emb_dim in enumerate(self.embeddings_dim):
+            if layer == 0:
+                self.first_h = Sequential(
+                    Linear(dim_features, out_emb_dim),
+                    BatchNorm1d(out_emb_dim),
+                    ReLU(),
+                    Linear(out_emb_dim, out_emb_dim),
+                    BatchNorm1d(out_emb_dim),
+                    ReLU())
+                self.linears.append(Linear(out_emb_dim, dim_target))
+            else:
+                input_emb_dim = self.embedding_dm[layer-1]
+                self.nns.append(
+                    Sequential(
+                        Linear(input_emb_dim, out_emb_dim),
+                        BatchNorm1d(out_emb_dim),
+                        ReLU(),
+                        Linear(out_emb_dim, out_emb_dim),
+                        BatchNorm1d(out_emb_dim),
+                        ReLU()))
+                self.convs.append(GinConv(self.nns[-1], train_eps(train_eps)))
+                self.linears.append(Linear(out_emb_dim, dim_target))
+
+        self.nns = torch.nn.ModuleList(self.nns)
+        self.convs = torch.nn.ModuleList(self.convs)
+        self.linears = torch.nn.ModuleList(self.linears)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        out = 0
+
+        for layer in range(self.no_layers):
+            if layer == 0:
+                x = self.firset_h(x)
+                out += F.dropout(self.pooling(
+                    self.linears[layer](x), batch), p=self.dropout)
+            else:
+                x = self.convs[layer-1](x, edge_index)
+                out += F.dropout(self.linears[layer](self.pooling(x, batch)),
+                                 p=self.dropout, training=self.training)
+        return out
