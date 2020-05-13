0fc4dbeb62540776f1f0dfbaa467a13fc71d22ce
Yang Liu <thomasliuy@gmail.com>
2020-05-13 13:27:39 -0400

    easy_gin runs succesfully

--- src/run_gin_easy.py
+++ src/run_gin_easy.py
@@ -2,28 +2,66 @@
 """
 import os.path as osp
 
-from slgnn.models.gcn.model import GIN
-from slgnn.data_processing.pyg_datasets import ZINCDataset
 from torch_geometric.data import DataLoader
 from torch.optim import Adam
-from torch.nn import BCEWithLogitsLoss
+from torch.nn import BCELoss
+import torch
+import matplotlib.pyplot as plt
 
+from slgnn.models.gcn.model import GIN_EASY
+from slgnn.data_processing.pyg_datasets import ZINCDataset
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
 
 root = osp.join("data", "ZINC", "graphs")
 name = "sampled_smiles_1000"
 dataset = ZINCDataset(root=root, name=name, use_node_attr=True)
-dataloader = DataLoader(dataset, batch_size=32)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-model = GIN(7, 740, )
-criterion = BCEWithLogitsLoss()
+dim_target = 740
+dim_features = dataset.data.x.size(1)
+
+model = GIN_EASY(dim_features, dim_target).to(device)
+criterion = BCELoss()
 optimizer = Adam(model.parameters(), lr=0.0001)
 
 epochs = 50
+model.train()
 for e in range(epochs):
     for i, batch in enumerate(dataloader):
+        batch = batch.to(device)
         out = model(batch)
-        loss = criterion(out, batch.y)
+        loss = criterion(out, batch.y.float())
         optimizer.zero_grad()
-        loss.back()
+        loss.backward()
         optimizer.step()
-        print("epoch: {}, batch: {}".format(e+1, i+1), end="\r")
+        print("epoch: {}, batch: {}, loss: {}".format(
+            e+1, i+1, loss.item()), end="\r")
+print()
+
+model.eval()
+label = batch.y[0].to("cpu").detach()
+out = model(batch)
+out_y = out[0].to("cpu").detach()
+fig, axes = plt.subplots(2, 1, figsize=(8., 12.))
+ax1, ax2 = axes.flatten()
+ax1.bar(list(range(out_y.shape[0])), out_y)
+ax1.set_xlabel("Reconstructed Fingerprint")
+ax2.bar(list(range(out_y.shape[0])), label)
+ax2.set_xlabel("PubChem Fingerprint")
+fig.savefig("./rec.png", dpi=300, bbox_inches="tight")
+
+label = batch.y[1].to("cpu").detach()
+out = model(batch)
+out_y = out[1].to("cpu").detach()
+fig, axes = plt.subplots(2, 1, figsize=(8., 12.))
+ax1, ax2 = axes.flatten()
+ax1.bar(list(range(out_y.shape[0])), out_y)
+ax1.set_xlabel("Reconstructed Fingerprint")
+ax2.bar(list(range(out_y.shape[0])), label)
+ax2.set_xlabel("PubChem Fingerprint")
+fig.savefig("./rec_2.png", dpi=300, bbox_inches="tight")
