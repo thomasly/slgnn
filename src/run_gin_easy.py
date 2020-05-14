89ebc7d190c23592810d147452ede35fa470b8bf
Yang Liu <thomasliuy@gmail.com>
2020-05-14 16:47:41 -0400

    add validation steps

--- src/run_gin_easy.py
+++ src/run_gin_easy.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 from slgnn.models.gcn.model import GIN_EASY
 from slgnn.data_processing.pyg_datasets import ZINCDataset
+from slgnn.training.utils import plot_train_val_losses
 
 
 if torch.cuda.is_available():
@@ -18,11 +19,12 @@ else:
     device = torch.device("cpu")
 
 root = osp.join("data", "ZINC", "graphs")
-name = "sampled_smiles_1000"
+name = "ZINC1k"
 dataset = ZINCDataset(root=root, name=name, use_node_attr=True)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+training_dataloader = DataLoader(dataset[:900], batch_size=32, shuffle=True)
+validating_dataloader = DataLoader(dataset[900:], batch_size=32, shuffle=False)
 
-dim_target = 740
+dim_target = dataset.data.y.size(1)
 dim_features = dataset.data.x.size(1)
 
 model = GIN_EASY(dim_features, dim_target).to(device)
@@ -31,21 +33,38 @@ optimizer = Adam(model.parameters(), lr=0.0001)
 
 epochs = 50
 model.train()
+training_losses = list()
+validating_losses = list()
 for e in range(epochs):
-    for i, batch in enumerate(dataloader):
+    for i, batch in enumerate(training_dataloader):
         batch = batch.to(device)
         out = model(batch)
-        loss = criterion(out, batch.y.float())
+        train_loss = criterion(torch.sigmoid(out), batch.y.float())
+        training_losses.append(train_loss.item())
+        with torch.no_grad():
+            counter = 0
+            val_batch_losses = list()
+            for val_batch in validating_dataloader:
+                val_batch = val_batch.to(device)
+                val_out = model(val_batch)
+                validate_loss = criterion(
+                    torch.sigmoid(val_out), val_batch.y.float())
+                val_batch_losses.append(validate_loss.item())
+                counter += 1
+            validating_losses.append(sum(val_batch_losses) / counter)
         optimizer.zero_grad()
-        loss.backward()
+        train_loss.backward()
         optimizer.step()
-        print("epoch: {}, batch: {}, loss: {}".format(
-            e+1, i+1, loss.item()), end="\r")
+        print("epoch: {}, batch: {}, train_loss: {}, val_loss: {}".format(
+            e+1, i+1, train_loss.item(), validating_losses[-1]), end="\r")
+    print()
 print()
+plot_train_val_losses(
+    training_losses, validating_losses, "train_val_losses.png")
 
 model.eval()
 label = batch.y[0].to("cpu").detach()
-out = model(batch)
+out = torch.sigmoid(model(batch))
 out_y = out[0].to("cpu").detach()
 fig, axes = plt.subplots(2, 1, figsize=(8., 12.))
 ax1, ax2 = axes.flatten()
@@ -56,7 +75,7 @@ ax2.set_xlabel("PubChem Fingerprint")
 fig.savefig("./rec.png", dpi=300, bbox_inches="tight")
 
 label = batch.y[1].to("cpu").detach()
-out = model(batch)
+out = torch.sigmoid(model(batch))
 out_y = out[1].to("cpu").detach()
 fig, axes = plt.subplots(2, 1, figsize=(8., 12.))
 ax1, ax2 = axes.flatten()
