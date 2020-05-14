3097e3d8179edf9740c48ab83f74f47f81867d6d
Yang Liu <thomasliuy@gmail.com>
2020-05-14 16:47:12 -0400

    plot training and validation losses

--- slgnn/training/utils.py
+++ slgnn/training/utils.py
@@ -16,3 +16,15 @@ def plot_reconstruct(decoder, encoder, features, adj, labels, index, output):
     ax2.bar(list(range(reconst.shape[1])), labels[index])
     ax2.set_xlabel("PubChem Fingerprint")
     fig.savefig(output, dpi=300, bbox_inches="tight")
+
+
+def plot_train_val_losses(train_losses: list, val_losses: list, output):
+    assert(len(train_losses) == len(val_losses))
+    fig, axe = plt.subplots(figsize=(8., 6.))
+    x = list(range(len(train_losses)))
+    axe.plot(x, train_losses, label="train_loss")
+    axe.plot(x, val_losses, label="val_loss")
+    axe.set_ylabel("BCE loss")
+    axe.set_xlabel("Steps")
+    axe.legend()
+    fig.savefig(output, dpi=300, bbox_inches="tight")
