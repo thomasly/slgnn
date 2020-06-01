fix roc_auc calculation

slgnn/data_processing/deepchem_datasets.py
@@ -38,7 +38,7 @@ class DeepchemDataset(InMemoryDataset, metaclass=ABCMeta):
             try:
                 x, edge_idx = self._graph_helper(smi)
                 data_list.append(Data(x=x, edge_index=edge_idx, y=y))
-            except AttributeError:
+            except AttributeError:  # SMILES invalid
                 continue
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
@@ -121,7 +121,7 @@ class BACE(DeepchemDataset):
     def _get_labels(self):
         df = pd.read_csv(self.raw_paths[0])
         for lb in df["Class"]:
-            yield torch.tensor([lb], dtype=torch.long)[None, :]
+            yield torch.tensor([lb], dtype=torch.long)
 
     def process(self, verbose=0):
         super().process(verbose)
@@ -165,7 +165,7 @@ class BBBP(DeepchemDataset):
     def _get_labels(self):
         df = pd.read_csv(self.raw_paths[0])
         for lb in df["p_np"]:
-            yield torch.tensor([lb], dtype=torch.long)[None, :]
+            yield torch.tensor([lb], dtype=torch.long)
 
     def process(self, verbose=0):
         super().process(verbose)
@@ -209,7 +209,7 @@ class ClinTox(DeepchemDataset):
     def _get_labels(self):
         df = pd.read_csv(self.raw_paths[0])
         for lb in df["CT_TOX"]:
-            yield torch.tensor([lb], dtype=torch.long)[None, :]
+            yield torch.tensor([lb], dtype=torch.long)
 
     def process(self, verbose=0):
         super().process(verbose)
@@ -256,7 +256,7 @@ class HIV(DeepchemDataset):
     def _get_labels(self):
         df = pd.read_csv(self.raw_paths[0])
         for lb in df["HIV_active"]:
-            yield torch.tensor([lb], dtype=torch.long)[None, :]
+            yield torch.tensor([lb], dtype=torch.long)
 
     def process(self, verbose=0):
         super().process(verbose)

slgnn/metrics/metrics.py
@@ -6,10 +6,18 @@ def _to_numpy(*tensors):
     return [t.cpu().detach() for t in tensors]
 
 
+def _one_class(target):
+    if len(target.size()) == 1:
+        return True
+    if target.size()[1] == 1:
+        return True
+    return False
+
+
 class Accuracy:
     def __call__(self, pred, target):
         pred, target = _to_numpy(pred, target)
-        if target.size()[1] == 1:
+        if _one_class(target):
             _, pred = pred.max(dim=1)
         else:
             pred = torch.round(torch.sigmoid(pred))
@@ -25,8 +33,8 @@ class ROC_AUC:
         self._last = 0
 
     def __call__(self, pred, target):
-        if target.size()[1] == 1:
-            pred = torch.log_softmax(pred)
+        if _one_class(target):
+            pred = torch.softmax(pred, 1)[:, 1]
         else:
             pred = torch.sigmoid(pred)
         pred, target = _to_numpy(pred, target)
@@ -44,7 +52,7 @@ class ROC_AUC:
 
 class F1:
     def __call__(self, pred, target):
-        if target.size()[1] == 1:
+        if _one_class(target):
             _, pred = pred.max(dim=1)
         else:
             pred = torch.round(torch.sigmoid(pred))