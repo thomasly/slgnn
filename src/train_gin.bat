60b89a7cccf57df9c1298240365214df07067d3e
Yang Liu <thomasliuy@gmail.com>
2020-06-10 22:38:08 -0400

    minor

--- src/train_gin.bat
+++ src/train_gin.bat
@@ -1,5 +1,5 @@
 python -m slgnn.training.train_gin ^
--c model_configs\config_GIN_Sider.yml
+-c model_configs\config_GIN_JAK2.yml
 
-REM -c model_configs\config_GIN_JAK1.yml
+REM -c model_configs\config_GIN_Sider.yml
 REM -c model_configs\config_GIN_Sider.yml
