421641bae7b9032c84aa9110ed591312098b45a8
Yang Liu <thomasliuy@gmail.com>
2020-07-06 11:27:29 -0400

    add train_gin.sh to repo

---
+++ src/train_gin.sh
@@ -0,0 +1,16 @@
+for conf in config_GIN_CPAN_JAK1.yml config_GIN_CPAN_JAK2.yml config_GIN_CPAN_JAK3.yml
+do
+    CUDA_VISIBLE_DEVICES=$1 \
+    python -m slgnn.training.train_gin \
+    -c model_configs/$conf
+done
+
+# CUDA_VISIBLE_DEVICES=$1 \
+# python -m slgnn.training.train_gin \
+# -c model_configs/config_GIN_CPAN_JAK3.yml &
+
+# for dataset in BACE BBBP ClinTox HIV
+# do
+#     echo $dataset
+#     CUDA_VISIBEL_DEVICES=$1 python -m slgnn.training.train_gin -c model_configs/config_GIN_$dataset.yml
+# done
