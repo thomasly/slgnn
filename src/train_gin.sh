b30f0ea7cea76ff024a788e019c1689c2412fcb7
Yang Liu <thomasliuy@gmail.com>
2020-07-06 11:32:32 -0400

    new train_gin.sh

--- src/train_gin.sh
+++ src/train_gin.sh
@@ -1,13 +1,13 @@
-for conf in config_GIN_CPAN_JAK1.yml config_GIN_CPAN_JAK2.yml config_GIN_CPAN_JAK3.yml
-do
-    CUDA_VISIBLE_DEVICES=$1 \
-    python -m slgnn.training.train_gin \
-    -c model_configs/$conf
-done
+# for conf in config_GIN_CPAN_JAK1.yml config_GIN_CPAN_JAK2.yml config_GIN_CPAN_JAK3.yml
+# do
+#     CUDA_VISIBLE_DEVICES=$1 \
+#     python -m slgnn.training.train_gin \
+#     -c model_configs/$conf
+# done
 
-# CUDA_VISIBLE_DEVICES=$1 \
-# python -m slgnn.training.train_gin \
-# -c model_configs/config_GIN_CPAN_JAK3.yml &
+CUDA_VISIBLE_DEVICES=$1 \
+python -m slgnn.training.train_gin \
+-c model_configs/config_GIN_CPAN_JAK$2.yml &
 
 # for dataset in BACE BBBP ClinTox HIV
 # do
