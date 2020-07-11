702f0f0583b5eeacfbe8298802aa76694398aa40
Yang Liu <thomasliuy@gmail.com>
2020-07-10 22:55:49 -0400

    run all tasks together

--- src/train_gin.sh
+++ src/train_gin.sh
@@ -5,19 +5,19 @@
 #     -c model_configs/$conf
 # done
 
-# for conf in config_GIN_BACE.yml config_GIN_BBBP.yml config_GIN_ClinTox.yml config_GIN_HIV.yml config_GIN_Sider.yml  config_GIN_CPAN_JAK1 config_GIN_CPAN_JAK2 config_GIN_CPAN_JAK3
-# do
-#     CUDA_VISIBLE_DEVICES=$1 \
-#     python -m slgnn.training.train_gin \
-#     -c model_configs/$conf
-# done
-
-for conf in config_GIN_Amu.yml config_GIN_Ellinger.yml config_GIN_Mpro.yml
+for conf in config_GIN_BACE.yml config_GIN_BBBP.yml config_GIN_ClinTox.yml config_GIN_HIV.yml config_GIN_Sider.yml config_GIN_CPAN_JAK1.yml config_GIN_CPAN_JAK2.yml config_GIN_CPAN_JAK3.yml config_GIN_Amu.yml config_GIN_Ellinger.yml config_GIN_Mpro.yml
 do
     CUDA_VISIBLE_DEVICES=$1 \
     python -m slgnn.training.train_gin \
     -c model_configs/$conf
 done
+
+# for conf in config_GIN_Amu.yml config_GIN_Ellinger.yml config_GIN_Mpro.yml
+# do
+#     CUDA_VISIBLE_DEVICES=$1 \
+#     python -m slgnn.training.train_gin \
+#     -c model_configs/$conf
+# done
 
 # CUDA_VISIBLE_DEVICES=$1 \
 # python -m slgnn.training.train_gin \
