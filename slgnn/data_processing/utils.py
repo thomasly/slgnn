dfeafabd60396c270af597010e79d1a8fec7e0c5
Yang Liu <thomasliuy@gmail.com>
2020-07-17 23:04:19 -0400

    fix_random_seed

--- slgnn/data_processing/utils.py
+++ slgnn/data_processing/utils.py
@@ -1,5 +1,26 @@
-from PyFingerprint.All_Fingerprint import get_fingerprint
+import random
 
 
 def get_pubchem_fingerprint(sm):
+    from PyFingerprint.All_Fingerprint import get_fingerprint
+
     return get_fingerprint(sm, fp_type="pubchem", output="vector")
+
+
+class fix_random_seed:
+
+    _seed = 0
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        random.seed(self._seed)
+        ret = self.func(*args, **kwargs)
+        random.seed()
+        return ret
+
+    @classmethod
+    def seed(cls, value: int):
+        cls._seed = value
+        return cls
--- slgnn/tests/test_data_processing.py
+++ slgnn/tests/test_data_processing.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import shutil
 import pickle as pk
+import random
 
 from scipy import sparse
 
@@ -9,16 +10,15 @@ from ..data_processing.zinc_to_hdf5 import ZincToHdf5, Hdf5Loader
 from ..data_processing import zinc_to_graph as zg
 from ..data_processing.pyg_datasets import JAK1
 from ..data_processing.pyg_datasets import ZINC1k
+from ..data_processing.utils import fix_random_seed
 from torch_geometric.data import DataLoader
 
 
 class TestZincReading(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.invalid_gzips = [f.path for f in os.scandir("test_data")]
-        cls.gzips = [
-            f.path for f in os.scandir("test_data") if f.name.endswith("gz")]
+        cls.gzips = [f.path for f in os.scandir("test_data") if f.name.endswith("gz")]
         cls.test_file_names = [
             "AAAARN.xaa.mol2.gz",
             "AAAARO.xaa.mol2.gz",
@@ -38,26 +38,21 @@ class TestZincReading(unittest.TestCase):
     def test_files_from_n_samples(self):
         if os.path.exists("test_data/index"):
             os.remove("test_data/index")
-        zth = ZincToHdf5.random_sample(
-            1000, dir_path="test_data", verbose=False)
+        zth = ZincToHdf5.random_sample(1000, dir_path="test_data", verbose=False)
         self.assertTrue(hasattr(zth, "_mol2s"))
         self.assertEqual(len(zth._mol2s), 1000)
         self.assertTrue(hasattr(zth, "_n_mols"))
         self.assertEqual(zth.n_mols, 1000)
         # number of samples too big
         with self.assertLogs() as cm:
-            zth = ZincToHdf5.random_sample(
-                10000, dir_path="test_data", verbose=False)
+            zth = ZincToHdf5.random_sample(10000, dir_path="test_data", verbose=False)
             self.assertLess(zth.n_mols, 10000)
             self.assertGreater(zth.n_mols, 0)
-        self.assertIn(
-            "does not have enough samples.", cm.output[0])
+        self.assertIn("does not have enough samples.", cm.output[0])
 
     def test_files_from_n_samples_wo_index(self):
         zth = ZincToHdf5.random_sample_without_index(
-            1000,
-            dir_path="test_data/splitted",
-            verbose=False
+            1000, dir_path="test_data/splitted", verbose=False
         )
         self.assertTrue(hasattr(zth, "_mol2s"))
         self.assertEqual(len(zth._mol2s), 1000)
@@ -66,11 +61,11 @@ class TestZincReading(unittest.TestCase):
         # number of samples too big
         with self.assertLogs() as cm:
             zth = ZincToHdf5.random_sample_without_index(
-                10000, dir_path="test_data/splitted", verbose=False)
+                10000, dir_path="test_data/splitted", verbose=False
+            )
             self.assertLess(zth.n_mols, 10000)
             self.assertGreater(zth.n_mols, 0)
-        self.assertIn(
-            "does not have enough samples.", cm.output[0])
+        self.assertIn("does not have enough samples.", cm.output[0])
 
     def test_indexing(self):
         self.assertIsInstance(ZincToHdf5.__dict__["indexing"], staticmethod)
@@ -84,21 +79,19 @@ class TestZincReading(unittest.TestCase):
         # assert the total number is there
         index["total"]
         # assert the number of entries is correct
-        index[index["total"]-1]
+        index[index["total"] - 1]
         with self.assertRaises(KeyError):
             index[index["total"]]
 
 
 class TestHdf5Loader(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         if os.path.exists("test_data/test.hdf5"):
             os.remove("test_data/test.hdf5")
         if os.path.exists("test_data/index"):
             os.remove("test_data/index")
-        zth = ZincToHdf5.random_sample(
-            1000, dir_path="test_data", verbose=False)
+        zth = ZincToHdf5.random_sample(1000, dir_path="test_data", verbose=False)
         zth.save_hdf5("test_data/test.hdf5")
         # cls.assertTrue(os.path.exists("test_data/test.hdf5"))
         cls.loader = Hdf5Loader("test_data/test.hdf5")
@@ -123,19 +116,16 @@ class TestHdf5Loader(unittest.TestCase):
 
 
 class TestGraphConverting(unittest.TestCase):
-
     def test_convert_sampled_smiles_to_graphs(self):
         inpath = "test_data/sampled_smiles_100.txt"
         pre = os.path.basename(inpath).split(".")[0]
         outpath = os.path.join(os.path.dirname(inpath), "graphs")
         zg.write_graphs(inpath, outpath, pre)
-        self.assertTrue(
-            os.path.isfile("test_data/graphs/sampled_smiles_100_A.txt"))
+        self.assertTrue(os.path.isfile("test_data/graphs/sampled_smiles_100_A.txt"))
         shutil.rmtree(outpath)
 
 
 class TestPygDatasets(unittest.TestCase):
-
     def test_JAK1_dataset(self):
         dataset = JAK1()
         dataloader = DataLoader(dataset, batch_size=32)
@@ -151,3 +141,27 @@ class TestPygDatasets(unittest.TestCase):
         self.assertEqual(data.num_graphs, 32)
         self.assertEqual(data.x.size()[1], 6)
         self.assertEqual(data.y.size(), (32, 740))
+
+
+class TestUtils(unittest.TestCase):
+    def test_fix_random_seed(self):
+        @fix_random_seed.seed(10)
+        def foo():
+            rand_nums = list()
+            for _ in range(10):
+                rand_nums.append(random.randint(0, 10))
+            return rand_nums
+
+        random_nums1 = foo()
+        random_nums2 = foo()
+        self.assertEqual(random_nums1, random_nums2)
+
+        def bar():
+            rand_nums = list()
+            for _ in range(10):
+                rand_nums.append(random.randint(0, 10))
+            return rand_nums
+
+        random_nums1 = bar()
+        random_nums2 = bar()
+        self.assertNotEqual(random_nums1, random_nums2)
