67787218c0914f36a5027f416ebb964c798c5884
Yang Liu <thomasliuy@gmail.com>
2020-08-04 15:16:23 -0400

    new experiments

--- src/train_gin.sh
+++ src/train_gin.sh
@@ -5,7 +5,7 @@
 #     -c model_configs/$conf
 # done
 
-for conf in HIV.yml
+for conf in Tox21.yml ToxCast.yml MUV.yml Sider.yml
 do
     CUDA_VISIBLE_DEVICES=$1 \
     python -m slgnn.training.train_gin \
