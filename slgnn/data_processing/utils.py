853da23d861f5a83fb7a54e3a432f772ac3d2289
Yang Liu <thomasliuy@gmail.com>
2020-09-16 12:11:41 -0400

    onehot transformer

--- slgnn/data_processing/utils.py
+++ slgnn/data_processing/utils.py
@@ -1,7 +1,96 @@
 import random
 import functools
 
+import torch
+from contextPred.chem.util import MaskAtom
+
 # import inspect
+
+
+class AtomFeaturesOneHotTransformer:
+    def __init__(
+        self,
+        num_atom_types=23,
+        num_degree=6,
+        num_formal_charge=5,
+        num_hybridization=8,
+        num_aromatic=2,
+        num_chirality=4,
+    ):
+        self.num_atom_types = num_atom_types
+        self.num_degree = num_degree
+        self.num_formal_charge = num_formal_charge
+        self.num_hybridization = num_hybridization
+        self.num_aromatic = num_aromatic
+        self.num_charality = num_chirality
+        self.feature_len = (
+            num_atom_types
+            + num_degree
+            + num_formal_charge
+            + num_hybridization
+            + num_aromatic
+            + num_chirality
+        )
+
+    def __call__(self, data):
+
+        oh_atom_type = torch.zeros((data.x.size(0), self.num_atom_types))
+        oh_degree = torch.zeros((data.x.size(0), self.num_degree))
+        oh_formal_charge = torch.zeros((data.x.size(0), self.num_formal_charge))
+        oh_hybridization = torch.zeros((data.x.size(0), self.num_hybridization))
+        oh_aromatic = torch.zeros((data.x.size(0), self.num_aromatic))
+        oh_charality = torch.zeros((data.x.size(0), self.num_charality))
+        features = [
+            oh_atom_type,
+            oh_degree,
+            oh_formal_charge,
+            oh_hybridization,
+            oh_aromatic,
+            oh_charality,
+        ]
+        for dt, feat in zip(data.x.T, features):
+            feat.scatter_(1, dt.view(-1, 1).type(torch.int64), 1)
+        data.x = torch.cat(features, 1)
+        return data
+
+
+class MaskOneHot:
+    def __init__(
+        self,
+        num_edge_type,
+        mask_rate,
+        num_atom_types=23,
+        num_degree=6,
+        num_formal_charge=5,
+        num_hybridization=8,
+        num_aromatic=2,
+        num_chirality=4,
+        mask_edge=True,
+    ):
+        num_atom_features = (
+            num_atom_types
+            + num_degree
+            + num_formal_charge
+            + num_hybridization
+            + num_aromatic
+            + num_chirality
+        )
+        self.oh_transformer = AtomFeaturesOneHotTransformer(
+            num_atom_types,
+            num_degree,
+            num_formal_charge,
+            num_hybridization,
+            num_aromatic,
+            num_chirality,
+        )
+        self.mask_transformer = MaskAtom(
+            num_atom_features, num_edge_type, mask_rate, mask_edge
+        )
+
+    def __call__(self, data):
+        data = self.oh_transformer(data)
+        data = self.mask_transformer(data)
+        return data
 
 
 def get_pubchem_fingerprint(smiles):
