4e0611e425f4f3375ce73e1eb9179e804350557a
Yang Liu <thomasliuy@gmail.com>
2020-10-19 14:57:34 -0400

    add chembl24 convert to ginfp

--- src/zinc_random_sample.py
+++ src/zinc_random_sample.py
@@ -8,8 +8,8 @@ from slgnn.data_processing.zinc_dataset import ZINC
 if __name__ == "__main__":
     fp_list = list()
     dataset = ZINC()
-    sampled_idx = sample(range(len(dataset)), k=10000)
+    sampled_idx = sample(range(len(dataset)), k=100000)
     for idx in sampled_idx:
         fp_list.append(dataset[idx].y.numpy().tolist())
-    with open(os.path.join("data", "zinc_random_fingerprints.pk"), "wb") as outf:
+    with open(os.path.join("data", "zinc_random_100000_fingerprints.pk"), "wb") as outf:
         pk.dump(fp_list, outf)
