c50d8e5b45a77de239b2421ffbf0c85aca8db49b
Yang Liu <thomasliuy@gmail.com>
2021-01-26 12:08:35 -0500

    remove src directory

--- src/contextpred_supervised_pretrain.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=$1 \
-    python -m contextPred.chem.pretrain_supervised \
-        --output_model_file ./contextPred/chem/trained_models/supervise_pretrained_model \
-        --input_model_file ./contextPred/chem/trained_models/context_pretrained_model_2
